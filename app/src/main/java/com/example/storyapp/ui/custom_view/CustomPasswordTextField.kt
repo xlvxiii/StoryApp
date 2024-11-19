c4f932da85932bd004a9eb07c38db003f8e33f31
xlvxiii <pangestualviansyah@gmail.com>
2024-11-20 02:07:02 +0700

    feat: add login page layout

---
+++ app/src/main/java/com/example/storyapp/ui/custom_view/CustomPasswordTextField.kt
@@ -0,0 +1,41 @@
+package com.example.storyapp.ui.custom_view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.example.storyapp.R
+
+class CustomPasswordTextField @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+
+    init {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) {
+                    if (s.toString().length < 8) {
+                        setError(context.getString(R.string.invalid_password), null)
+                    }
+                }
+            }
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+}
--- app/src/main/java/com/example/storyapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/login/LoginActivity.kt
@@ -1,25 +1,25 @@
-package com.example.storyapp
+package com.example.storyapp.ui.login
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.storyapp.ui.login.LoginActivity
+import com.example.storyapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
     }
 }
